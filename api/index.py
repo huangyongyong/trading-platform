336672b261b73ce49c407861fcca4b13a4dbc4b9
huangyongyong <2715935782@qq.com>
2026-05-25 12:46:44 +0800

    fix: final attempt to fix vercel.json for Vercel7

--- api/index.py
+++ api/index.py
@@ -3,4 +3,8 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.api import app
 # 确保 Vercel 能找到 app 对象
-app = app
+#app = app
+# 不要在这里导入 app，而是定义一个函数，在运行时才导入
+def app(environ, start_response):
+    from app.api import app as fastapi_app
+    return fastapi_app(environ, start_response)
